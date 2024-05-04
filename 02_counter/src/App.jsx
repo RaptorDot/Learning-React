53fb434d556dafa58ca1528e31e43a775c5ef4d2
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-05-04 22:09:07 +0530

    hooks

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  
+         let [counter , setCounter]  =  useState(1)
+  
+  const addValue = ()=>{
+    counter = counter +1;
+    setCounter(counter);
+    console.log('value added' , counter)
+  }
+
+  const removeValue = ()=>{
+    if(counter>0){
+      counter -= 1;
+    }
+      setCounter(counter);
+      console.log('after removing the value is: ' , counter);
+  }
+
+  // let count = 51;
+  return (
+    <>
+     <h1>Learning React</h1>
+    <h2>counter value {counter}</h2>
+      <button
+      onClick={addValue}
+      >add value {counter}</button>
+
+      <br />
+
+      <button
+      onClick={removeValue}
+      >remove value</button>
+    <p>footter: {counter}</p>
+    </>
+  )
+}
+
+export default App
