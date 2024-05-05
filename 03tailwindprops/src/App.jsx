9d1e7100aad402ecd720e7b2653e7f36aa06b818
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-05-05 17:18:42 +0530

    props and tailwinds

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -4,13 +4,11 @@
 import Card from './assets/components/card'
 import './App.css'
 
-
 function App() {
   let myObj = {
     username: 'Debasish',
     age: 18
   }
-
   return (
     <>
   <h1 className='bg-green-400 text-black p-4 rounded-xl mb-3' >tailwind test</h1>
