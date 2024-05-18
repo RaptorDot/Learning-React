48072454a1905202813e443f55ea345df83de708
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-05-18 21:08:24 +0530

    rout

--- 07routernreact/src/main.jsx
+++ 07routernreact/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
@@ -30,15 +30,15 @@ import Contact from './components/Contact.jsx'
   // ])
   //Alternate Way
 
-    const router = createBrowserRouter(
-      createRoutesFromElements(
-        <Route path = '/' element = {<Layout/>}>
-            <Route path = '' element = {<Home/>}/>
-            <Route path = 'about' element = {<About/>}/>
-            <Route path = 'contact' element = {<Contact/>}/>
-        </Route>
-      )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+      </Route>
     )
+  )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
