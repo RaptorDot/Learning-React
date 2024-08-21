915955aeb8f98db7471f864cfe1e068ed6649032
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-08-21 17:42:02 +0530

    todo in progress

--- 10localToDOwithContext/src/App.jsx
+++ 10localToDOwithContext/src/App.jsx
@@ -4,12 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoContextProvider } from './contexts';
 
+
 function App() {
   const [todo, setTodo] = useState([]);
 
+   const addtodo = (todo)=>{
+          setTodo((p)=>[{id: Date.now() ,...todo},...p])
+    }
+    const updateTodo = (id ,todo)=>{
+      setTodo((prev)=>prev.map((prevT)=>prevT.id===id? todo: prevT))
+    }
+
+
   return (
-  <TodoContextProvider value = {{}}>
-  <div className="bg-[#172842] min-h-screen py-8">
+  <TodoContextProvider value = {{todos, addtodo, updateTodo, deleteTodo, toggleComplete}} >
+<div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
             <div className="mb-4">
